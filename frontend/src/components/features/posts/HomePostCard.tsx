1236ee25149f7432c004a9a8fb4d05e8e40aa2f8
ioannis-voda <ioannis.maragkakis@voda.ai>
2026-05-03 19:16:18 +0300

    fix a bug that posts were not rendering in safari

--- frontend/src/components/features/posts/HomePostCard.tsx
+++ frontend/src/components/features/posts/HomePostCard.tsx
@@ -1,5 +1,5 @@
 import type { Post } from '@/types/posts'
-import { Card, Divider } from '@mui/material'
+import { Card, Divider, Box } from '@mui/material'
 import HomePostCardActions from '@/components/features/posts/HomePostCardActions'
 import HomePostCardBody from '@/components/features/posts/HomePostCardBody'
 import HomePostCardTitle from '@/components/features/posts/HomePostCardTitle'
@@ -10,22 +10,23 @@ interface PostProps {
 
 export default function HomePostCard({ post }: PostProps) {
   return (
-    <Card
-      variant='outlined'
-      sx={{
-        borderColor: 'divider',
-        borderRadius: 4,
-        padding: { xs: 3, sm: 4 },
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 2,
-        minHeight: 'min-content',
-      }}
-    >
-      <HomePostCardTitle user={post.user} post={post} />{' '}
-      <Divider sx={{ borderColor: 'divider' }} />
-      <HomePostCardBody post={post} />
-      <HomePostCardActions post={post} />
-    </Card>
+    <Box sx={{ height: 'min-content' }}>
+      <Card
+        variant='outlined'
+        sx={{
+          borderColor: 'divider',
+          borderRadius: 4,
+          padding: { xs: 3, sm: 4 },
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 2,
+        }}
+      >
+        <HomePostCardTitle user={post.user} post={post} />{' '}
+        <Divider sx={{ borderColor: 'divider' }} />
+        <HomePostCardBody post={post} />
+        <HomePostCardActions post={post} />
+      </Card>
+    </Box>
   )
 }
